f84c0914c59c22b4ddcf5141d8293083eb837f04
vanz1220 <ije.malong@gmail.com>
2022-04-20 11:19:30 +0800

    IBL Custom Block

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '98b980b9f3aa5af41679a3f757f87a9c');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '3b478d337d6b27e16c16ff96c6a36c8a');
